b6006d0b39f2f0d48f5e8f6e4863fc5256447c73
Erick Marques <erickmarques43@gmail.com>
2021-07-22 19:59:37 -0300

    second commit

--- src/primeiroProjeto/PrimeiroProjeto.java
+++ src/primeiroProjeto/PrimeiroProjeto.java
@@ -1,10 +1,13 @@
 package primeiroProjeto;
 
+import java.util.Scanner;
+
 public class PrimeiroProjeto {
 
 	public static void main(String[] args) {
 		String texto = "Vamos começar a programar em Java";
 		System.out.println(texto);
+		Scanner sc = new Scanner(System.in);
 		}
 
 }
